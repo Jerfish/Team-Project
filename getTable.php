389d35f8208001eb2e55ef44bc1fd1ce93375a3b
BlueGandalf <pksmith137@gmail.com>
2018-02-11 19:05:24 +0000

    cleaned up and added sort

--- getTable.php
+++ getTable.php
@@ -21,14 +21,24 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
-    
+
     $str = '<input type="text" id="'.$type.'Search" onkeyup="filterTable('.$type.')" placeholder="Search..." style="width: 20%;">';
 
     if($type == "Problem"){
-        $str .= "<table><tr><th>Problem ID</th><th>Date/Time Opened</th><th>ID of Caller</th><th>ID of Operator</th><th>Hardware/Software</th><th>Problem Type</th><th>Specialist ID</th><th>Date/Time Solved</th><th>Status</th></tr>";
+        $str .= "<table id='problemTable'><thead><tr>";
+        $str .= "<th onclick='sortTable(0, \"problemTable\")'>Problem ID</th>";
+        $str .= "<th onclick='sortTable(1, \"problemTable\")'>Date/Time Opened</th>";
+        $str .= "<th onclick='sortTable(2, \"problemTable\")'>ID of Caller</th>";
+        $str .= "<th onclick='sortTable(3, \"problemTable\")'>ID of Operator</th>";
+        $str .= "<th onclick='sortTable(4, \"problemTable\")'>Hardware/Software</th>";
+        $str .= "<th onclick='sortTable(5, \"problemTable\")'>Problem Type</th>";
+        $str .= "<th onclick='sortTable(6, \"problemTable\")'>Specialist ID</th>";
+        $str .= "<th onclick='sortTable(7, \"problemTable\")'>Date/Time Solved</th>";
+        $str .= "<th onclick='sortTable(8, \"problemTable\")'>Status</th>";
+        $str .= "</tr></thead><tbody>";
         while($row = $result->fetch_assoc()) {
             $str .= "<tr><td>".$row["ProblemID"]."</td><td>".$row["CallDateTime"]."</td><td>".$row["CallerID"]."</td><td>".$row["OperatorID"]."</td><td>";
-            
+
             if($row["HardwareID"] == 0){
                 $str .= "Software";
             }else if($row["SoftwareID"] == 0){
@@ -36,10 +46,10 @@ if ($result->num_rows > 0) {
             }else{
                 $str .= "Error";
             }
-                
-               $str .= "</td><td>".$row["ProblemType"]."</td><td>".$row["SpecialistID"]."</td><td>".$row["DateTime Solved"]."</td><td>".$row["Status"]."</td></tr>";
+
+            $str .= "</td><td>".$row["ProblemType"]."</td><td>".$row["SpecialistID"]."</td><td>".$row["DateTime Solved"]."</td><td>".$row["Status"]."</td></tr>";
         }
-        $str .= '</table>';
+        $str .= '</tbody></table>';
         echo $str;
     }
 } else {
