4ca79be64977d5ca9f7eb6be251a3f5902388427
MrRibena <33399158+MrRibena@users.noreply.github.com>
2018-02-13 00:03:07 +0000

    Create getHardTable.php

---
+++ getHardTable.php
@@ -0,0 +1,30 @@
+<?php
+$type = $_REQUEST['type'];
+$servername = "localhost";
+include "team11-mysql-connect.php"; //to provide $username,$password
+$dbname = "team11project";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+if($type == "problem"){
+    $sql = "SELECT  `Hardware ID` ,  `ProblemType` ,  `CallDateTime` ,  `DateTime Solved` FROM  `ProblemInfo` HAVING  `Hardware ID` !=0";
+}else{
+    echo "this will work later";
+}
+$result = $conn->query($sql);
+//$value = json_encode($result->fetch_all());
+$num = mysql_num_rows($result);
+for ($i = 0; $i < $num; $i++)
+{
+    $hardwareArray[] = mysql_fetch_assoc($result);
+}
+        echo $hardwareArray[];
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
