17e0be7524584f611ccc9e8704aab23ff77caa58
BlueGandalf <pksmith137@gmail.com>
2018-02-14 18:25:35 +0000

    added jQuery to make the tables work

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 <html>
     <head>
         <title>Make-it-all Helpdesk</title>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
         <script>
             
             function filterTable(i){
