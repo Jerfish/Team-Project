5ede7b8ce59853b599164d4667ddd59361d23c72
MrRibena <33399158+MrRibena@users.noreply.github.com>
2018-02-15 22:19:51 +0000

    aghg

--- getHardTable.php
+++ getHardTable.php
@@ -8,7 +8,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT Hardware.HardwareID, Hardware.HardwareName, ProblemInfo.CallDateTime, ProblemInfo.DateTimeSolved FROM Hardware JOIN ProblemInfo ON Hardware.HardwareID = ProblemInfo.HardwareID WHERE ProblemInfo.DateTimeSolved != \"0000-00-00 00:00:00\" AND Hardware.HardwareID >0 ORDER BY Hardware.HardwareID";
+$sql = "SELECT Hardware.HardwareID, Hardware.HardwareType, ProblemInfo.CallDateTime, ProblemInfo.DateTimeSolved FROM Hardware JOIN ProblemInfo ON Hardware.HardwareID = ProblemInfo.HardwareID WHERE ProblemInfo.DateTimeSolved != \"0000-00-00 00:00:00\" AND Hardware.HardwareID >0 ORDER BY Hardware.HardwareID";
 $result = $conn->query($sql);
 $rows = [];
 while ($row = $result->fetch_assoc()) {
