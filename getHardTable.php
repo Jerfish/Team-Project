079835a3770c5043d2ae1990360911b7af6d1bc5
MrRibena <33399158+MrRibena@users.noreply.github.com>
2018-02-15 21:57:17 +0000

    Update getHardTable.php

--- getHardTable.php
+++ getHardTable.php
@@ -1,5 +1,4 @@
 <?php
-$type = $_REQUEST['type'];
 $servername = "localhost";
 include "team11-mysql-connect.php"; //to provide $username,$password
 $dbname = "team11project";
@@ -8,23 +7,13 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
-if($type == "problem"){
-    $sql = "SELECT  `Hardware ID` ,  `ProblemType` ,  `CallDateTime` ,  `DateTime Solved` FROM  `ProblemInfo` HAVING  `Hardware ID` !=0";
-}else{
-    echo "this will work later";
 }
+$sql = "SELECT Hardware.HardwareID, Hardware.HardwareName, ProblemInfo.CallDateTime, ProblemInfo.DateTimeSolved FROM Hardware JOIN ProblemInfo ON Hardware.HardwareID = ProblemInfo.HardwareID WHERE ProblemInfo.DateTimeSolved != \"0000-00-00 00:00:00\" AND Hardware.HardwareID >0 ORDER BY Hardware.HardwareID";
 $result = $conn->query($sql);
-//$value = json_encode($result->fetch_all());
-$num = mysql_num_rows($result);
-for ($i = 0; $i < $num; $i++)
-{
-    $hardwareArray[] = mysql_fetch_assoc($result);
+$rows = [];
+while ($row = $result->fetch_assoc()) {
+    $rows[] = $row;
 }
-        echo $hardwareArray[];
-    }
-} else {
-    echo "0 results";
-}
+echo json_encode($rows);
 $conn->close();
 ?>
