08ad10f34f40f0d4b6f919823b510a21eeb2e9e9
MrRibena <33399158+MrRibena@users.noreply.github.com>
2018-02-17 23:36:42 +0000

    added unsolved problem count

--- getSpecTable.php
+++ getSpecTable.php
@@ -8,14 +8,14 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT Personnel.Name, COUNT(ProblemInfo.SolvedByID) as ProblemCount FROM Personnel JOIN Specialists ON  Personnel.PersonnelID = Specialists.PersonnelID JOIN ProblemInfo ON Specialists.SpecialistID = ProblemInfo.SolvedByID WHERE ProblemInfo.SolvedByID > 0 GROUP BY Personnel.Name";
+$sql = "SELECT Personnel.Name, COUNT(ProblemInfo.SolvedByID) as SolvedCount FROM Personnel JOIN Specialists ON  Personnel.PersonnelID = Specialists.PersonnelID JOIN ProblemInfo ON Specialists.SpecialistID = ProblemInfo.SolvedByID WHERE ProblemInfo.SolvedByID > 0 GROUP BY Personnel.Name";
 $result = $conn->query($sql);
 $rows = [];
 while ($row = $result->fetch_assoc()) {
     $rows[] = $row;
 }
 
-$sql2="SELECT Personnel.Name, COUNT(ProblemInfo.SolvedByID) FROM `ProblemInfo` JOIN Specialists ON Specialists.SpecialistID = ProblemInfo.SpecialistID JOIN Personnel ON Personnel.PersonnelID = Specialists.SpecialistID WHERE ProblemInfo.SpecialistID !=ProblemInfo.SolvedByID GROUP BY Personnel.Name";
+$sql2="SELECT Personnel.Name, COUNT(ProblemInfo.SolvedByID) as UnsolvedCount FROM `ProblemInfo` JOIN Specialists ON Specialists.SpecialistID = ProblemInfo.SpecialistID JOIN Personnel ON Personnel.PersonnelID = Specialists.SpecialistID WHERE ProblemInfo.SpecialistID !=ProblemInfo.SolvedByID GROUP BY Personnel.Name";
 $result = $conn->query($sql2);
 while ($row = $result->fetch_assoc()) {
     $rows[] = $row;
